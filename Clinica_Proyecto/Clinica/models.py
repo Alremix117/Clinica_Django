d3c1cedfeae9b1f31fe9d0618799ff618d373472
AndrelGanShiTV <andresfelipegiraldoandrade@gmail.com>
2025-10-18 09:21:31 -0500

    Interface de modelos

--- Clinica_Proyecto/Clinica/models.py
+++ Clinica_Proyecto/Clinica/models.py
@@ -3,3 +3,54 @@ from django.db import models
 # Create your models here.
 class Paciente(models.Model):
     pass
+
+class Discapacidad(models.Model):
+    pass
+
+class Paciente_Discapacidad(models.Model):
+    pass
+
+class Oposicion_Donacion(models.Model):
+    pass
+
+class Ocupacion(models.Model):
+    pass
+
+class Municipio(models.Model):
+    pass
+
+class Etnia(models.Model):
+    pass
+
+class Comunidad_Etnica(models.Model):
+    pass
+
+class Pais(models.Model):
+    pass
+
+class Paciente_Pais(models.Model):
+    pass
+
+class Voluntad_Anticipada(models.Model):
+    pass
+
+class Entidad_Prestadora_Salud(models.Model):
+    pass
+
+class Contacto_Servicio_Salud(models.Model):
+    pass
+
+class Modalidad_Realizacion_Tecnologia_Salud(models.Model):
+    pass
+
+class Via_Ingreso_Servicio_Salud(models.Model):
+    pass
+
+class Motivo_Atencion(models.Model):
+    pass
+
+class Diagnostico(models.Model):
+    pass
+
+class Enfermedad_Huerfana(models.Model):
+    pass
