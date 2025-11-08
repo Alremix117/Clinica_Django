638b5e34e1324c8d4bc5a0da5512fb3a68e2f7c0
andrelganshitv@gmail.com <andrelganshitv@gmail.com>
2025-11-08 07:48:23 -0500

    Views DELETE, UPDATE

--- Clinica_Proyecto/Clinica/views.py
+++ Clinica_Proyecto/Clinica/views.py
@@ -34,6 +34,57 @@ def crear_paciente(request):
         "form_discapacidad": form_discapacidad,
     })
 
+@transaction.atomic
+def paciente_edit(request, id):
+    paciente = get_object_or_404(Paciente, paciente_UUID=id)
+
+    if request.method == "POST":
+        form_paciente = FormPaciente(request.POST, instance=paciente)
+        form_nacionalidad = FormNacionalidad(request.POST)
+        form_discapacidad = FormDiscapacidad(request.POST)
+
+        if form_paciente.is_valid() and form_nacionalidad.is_valid() and form_discapacidad.is_valid():
+            form_paciente.save()
+
+            # Limpiar relaciones existentes
+            Paciente_Pais.objects.filter(paciente_UUID=paciente).delete()
+            Paciente_Discapacidad.objects.filter(paciente_UUID=paciente).delete()
+
+            # Volver a guardar relaciones seleccionadas
+            for pais in form_nacionalidad.cleaned_data['paises']:
+                Paciente_Pais.objects.create(paciente_UUID=paciente, codigo_pais=pais)
+
+            for disc in form_discapacidad.cleaned_data['discapacidades']:
+                Paciente_Discapacidad.objects.create(paciente_UUID=paciente, id_discapacidad=disc)
+
+            return redirect("paciente_detail", id=paciente.paciente_UUID)
+
+    else:
+        nacionalidades_actuales = paciente.nacionalidad.values_list('codigo_pais', flat=True)
+        discapacidades_actuales = paciente.discapacidades.values_list('id_discapacidad', flat=True)
+
+        form_paciente = FormPaciente(instance=paciente)
+        form_nacionalidad = FormNacionalidad(initial={'paises': nacionalidades_actuales})
+        form_discapacidad = FormDiscapacidad(initial={'discapacidades': discapacidades_actuales})
+
+    return render(request, "pacientes/paciente_edit.html", {
+        "form_paciente": form_paciente,
+        "form_nacionalidad": form_nacionalidad,
+        "form_discapacidad": form_discapacidad,
+        "paciente": paciente,
+    })
+
+def paciente_delete(request, id):
+    paciente = get_object_or_404(Paciente, paciente_UUID=id)
+
+    if request.method == "POST":
+        paciente.delete()
+        return redirect("paciente_list")
+
+    return render(request, "pacientes/paciente_eliminar_confirmacion.html", {
+        "paciente": paciente
+    })
+
 def paciente_list(request):
     pacientes = Paciente.objects.all()
     return render(request, "pacientes/paciente_list.html", {"pacientes": pacientes})
@@ -48,3 +99,4 @@ def paciente_detail(request, id):
         "nacionalidades": nacionalidades,
         "discapacidades": discapacidades,
     })
+
